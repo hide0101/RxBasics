8cb417e6ab20923d6cde2d5d6eec2c812a5e2bfd
Hideaki Komori <hideakikomori0101@gmail.com>
2018-06-03 14:16:34 +0900

    Add scan and buffer

--- RxBasics/ViewController.swift
+++ RxBasics/ViewController.swift
@@ -120,24 +120,42 @@ class ViewController: UIViewController {
 //                })
 //                .dispose()
 //        }
-        executeProcedure(for: "flatMap and flatMapLatest") {
-            struct GamePlayer {
-                let playerScore: Variable<Int>
-            }
+//        executeProcedure(for: "flatMap and flatMapLatest") {
+//            struct GamePlayer {
+//                let playerScore: Variable<Int>
+//            }
+//            let disposeBag = DisposeBag()
+//            let alex = GamePlayer(playerScore: Variable(70))
+//            let gemmea = GamePlayer(playerScore: Variable(85))
+//            var currentPlayer = Variable(alex)
+//            currentPlayer.asObservable()
+//                .flatMapLatest( { $0.playerScore.asObservable() })
+//                .subscribe(onNext: {
+//                    print($0)
+//                })
+//                .disposed(by: disposeBag)
+//            currentPlayer.value.playerScore.value = 90
+//            alex.playerScore.value = 95
+//            currentPlayer.value = gemmea
+//            alex.playerScore.value = 96
+//        }
+        executeProcedure(for: "scan and buffer") {
             let disposeBag = DisposeBag()
-            let alex = GamePlayer(playerScore: Variable(70))
-            let gemmea = GamePlayer(playerScore: Variable(85))
-            var currentPlayer = Variable(alex)
-            currentPlayer.asObservable()
-                .flatMapLatest( { $0.playerScore.asObservable() })
+            let gameScore = PublishSubject<Int>()
+            gameScore.buffer(timeSpan: 0.0, count: 3, scheduler: MainScheduler.instance)
+                .map{
+                    print($0, "-->", terminator: "")
+                    return $0.reduce(0, +)
+                }
+                .scan(501, accumulator: -)
+                .map({max($0, 0)})
                 .subscribe(onNext: {
                     print($0)
                 })
                 .disposed(by: disposeBag)
-            currentPlayer.value.playerScore.value = 90
-            alex.playerScore.value = 95
-            currentPlayer.value = gemmea
-            alex.playerScore.value = 96
+            gameScore.onNext(60)
+            gameScore.onNext(13)
+            gameScore.onNext(50)
         }
     }
 
